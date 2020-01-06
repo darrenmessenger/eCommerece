0e2f87245df5a85fa16a7d9f1fcae1066b9e9cdc
Ubuntu <ubuntu@ip-172-31-85-70.ec2.internal>
2020-01-06 21:54:19 +0000

    checkout views

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,3 +1,4 @@
+
 """ecommerce URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -18,6 +19,7 @@ from accounts import urls as urls_accounts
 from products import urls as urls_products
 from cart import urls as urls_cart
 from search import urls as urls_search
+from checkout import urls as urls_checkout
 from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
@@ -28,6 +30,7 @@ urlpatterns = [
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
